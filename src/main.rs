8fdb179ea3c143c6633fd1e605e6929089f21210
Arthur MATTHYS <AMY@dimosoftware.com>
2023-12-01 15:36:02 +0100

    AoC(day00): done

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+mod year2023;
+
+use std::fs;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// Year of the program
+    #[arg(short, long)]
+    pub year: u32,
+
+    /// Day of the program
+    #[arg(short, long)]
+    pub day: u8,
+
+    /// Exercice number
+    #[arg(short, long)]
+    pub exercice: u8,
+
+    /// path to the file
+    #[arg(short, long)]
+    pub filename: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let file = fs::read(args.filename).expect("cannot open file");
+
+    let content = std::str::from_utf8(&file).expect("cannot convert to utf8");
+
+    println!(
+        "result : {}",
+        match (args.year, args.day, args.exercice) {
+            (2023, 0, 0) => year2023::day01::eval_file(content),
+            // (0, 1) => day_01::top_three(content),
+            // (1, 0) => day_02::count_points(content),
+            // (1, 1) => day_02::count_points_with_result(content),
+            // (2, 0) => day_03::get_priorities(content),
+            // (2, 1) => day_03::get_badges(content),
+            // (3, 0) => day_04::get_pairs_number(content),
+            // (3, 1) => day_04::get_overlaps(content),
+            _ => unreachable!(),
+        }
+    );
+}
---
+++ src/year2023/day01/mod.rs
@@ -0,0 +1,72 @@
+const NUMBERS: [&str; 10] = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+fn line_value(line: &str) -> i32 {
+    let first = line.find(char::is_numeric).unwrap_or(usize::MAX);
+    let first_nbr_idx = NUMBERS
+        .iter()
+        .enumerate()
+        .filter_map(|(nbr, repr)| line.find(repr).map(|idx_find| (nbr, idx_find)))
+        .min_by_key(|x| x.1)
+        .unwrap_or((0, usize::MAX));
+    let first_nbr = if first_nbr_idx.1 < first {
+        first_nbr_idx.0 as i32
+    } else {
+        line.get(first..first + 1)
+            .expect("index of str")
+            .parse::<i32>()
+            .expect("figure can be parsed")
+    };
+    let last = line.rfind(char::is_numeric).unwrap_or(0);
+    let last_nbr_idx = NUMBERS
+        .iter()
+        .enumerate()
+        .filter_map(|(nbr, repr)| line.rfind(repr).map(|idx_find| (nbr, idx_find)))
+        .max_by_key(|x| x.1)
+        .unwrap_or((0, 0));
+    let last_nbr = if last_nbr_idx.1 > last {
+        last_nbr_idx.0 as i32
+    } else {
+        line.get(last..last + 1)
+            .expect("index of str")
+            .parse::<i32>()
+            .expect("figure can be parsed")
+    };
+
+    first_nbr * 10 + last_nbr
+}
+
+pub(crate) fn eval_file(file: &str) -> i32 {
+    file.split_whitespace()
+        .fold(0, |acc, line| acc + line_value(line))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::eval_file;
+
+    #[test]
+    fn first_test() {
+        let v = r#"
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"#;
+        assert_eq!(142, eval_file(v));
+    }
+
+    #[test]
+    fn second_test() {
+        let v = r#"
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"#;
+        assert_eq!(281, eval_file(v));
+    }
+}
